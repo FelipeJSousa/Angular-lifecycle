9de6636159ea5fc888db27e503d404c385cac981
Felipe Sousa <csfelipe8@gmail.com>
2021-03-27 13:37:33 -0300

    Added event hooks

--- src/app/main-cycle/child-cycle/child-cycle.component.ts
+++ src/app/main-cycle/child-cycle/child-cycle.component.ts
@@ -1,21 +1,40 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChange, OnChanges ,OnDestroy, AfterContentInit} from '@angular/core';
 
 @Component({
   selector: 'app-child-cycle',
   templateUrl: './child-cycle.component.html',
   styleUrls: ['./child-cycle.component.css']
 })
-export class ChildCycleComponent implements OnInit {
+export class ChildCycleComponent implements OnInit, OnChanges, OnDestroy, AfterContentInit{
   
   @Input() name : string;
   @Input() age : number;
   @Input() food : string;
   
   constructor() {
-    console.log(`${this.name} - ${this.age} - ${this.food}`)
-   }
-
+    console.log(`constructor: ${this.name} - ${this.age} - ${this.food}`)
+  }
+  
   ngOnInit(): void {
+    console.log(`OnInit: ${this.name} - ${this.age} - ${this.food}`)
+  }
+  
+  ngAfterContentInit(){
+    console.log(`AfterContentInit: ${this.name} - ${this.age} - ${this.food}`)
+  }
+
+  ngAfterViewInit(){
+    console.log(`AfterViewInit: ${this.name} - ${this.age} - ${this.food}`)
+  }
+
+  ngOnChanges(changes: {[propKey : string] : SimpleChange}){
+    console.log(`OnChanges: ${this.name} - ${this.age} - ${this.food}`)
+  }
+  
+  
+  ngOnDestroy(){
+    console.log(`OnDestroy: ${this.name} - ${this.age} - ${this.food}`)
+
   }
 
 
