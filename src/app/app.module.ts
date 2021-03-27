c273bd52b27eecf0acca689a68ee6d58786be8d2
Felipe Sousa <csfelipe8@gmail.com>
2021-03-27 08:07:47 -0300

    recovered files before lifecycle-child

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,19 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatSelectModule } from '@angular/material/select'
 import { MatButtonModule } from '@angular/material/button'
 import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatListModule } from '@angular/material/list'
 
 import { AppComponent } from './app.component';
 import { MainCycleComponent } from './main-cycle/main-cycle.component';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { lifecycleChildComponent } from './main-cycle/lifecycle-child/lifecycle-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainCycleComponent
+    MainCycleComponent,
+    lifecycleChildComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +30,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatButtonModule, 
     MatFormFieldModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
